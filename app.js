47ca967af5362a423005dff4827e82017d70751c
Danial95-prog <danialdurrani95@gmail.com>
2025-09-15 02:15:22 +0800

    2nd-commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//console.log(9);
+let flippedCards = [];
+let matchedCards = [];
+
+function startGame(difficulty) {
+  console.log(`Starting game with difficulty: ${difficulty}`);}
+
+  const board = document.getElementById('game-board');
+  board.innerHTML = '';
+  flippedCards = [];
+  matchedCards = [];
